beea6af3848b66866f4f67584c8f9c4c8c6da00b
Jogis <ronipihajoki@proton.me>
2025-11-13 20:08:03 +0200

    Update App.java

--- CodeRunner1-1/src/App.java
+++ CodeRunner1-1/src/App.java
@@ -1,5 +1,27 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        car olio1 = new car();
+    }
+} // Main loppu
+class car{
+    public String brand;
+    public String model;
+    public int amountOfFuel;
+    
+    public void brake(){
+        System.out.println("The car is breaking");
+    }
+
+    public void accelerate(){
+        amountOfFuel --;
+        if(amountOfFuel > 0){
+            System.out.println("Car is accelerating");
+        }
+    }
+
+    public void printData(){
+        System.out.println("Brand: " + brand);
+        System.out.println("Model: " + model);
+        System.out.println("Fuel: " + amountOfFuel);
     }
 }
