f0821b562cc9edb927994833a9035ec863a86724
Jogis <ronipihajoki@proton.me>
2025-11-13 22:05:47 +0200

    Update App.java

--- CodeRunner1-1/src/App.java
+++ CodeRunner1-1/src/App.java
@@ -1,12 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Car olio1 = new Car("Kia", "Rio", 2);
-
-        olio1.accelerate();
-        olio1.brake();
-        olio1.printData();
-    }
-} // Main loppu
+Car car = new Car("Opel","Astra",50);
+car.setModel("Corsa");
+System.out.println(car.getModel());
+car.refuel(5);
+}
+}// Main loppu
 /* Harjoitus 1
 class Car{
     public String brand;
@@ -31,6 +30,7 @@ class Car{
     }
 }
 */
+/* Harjoitus 2
 class Car{
     public String brand;
     public String model;
@@ -64,3 +64,78 @@ class Car{
         System.out.println("Fuel: " + amountOfFuel);
     }
 }
+*/
+/* Harjoitus 3
+class Car{
+    private String brand;
+
+    private String model;
+    public void setModel(String model){
+        this.model = model;
+    }
+    public String getModel(){
+        return model;
+    }
+
+    private int amountOfFuel;
+
+    public Car(String b, String m, int aOF){ // Parameterized
+        brand = b;
+        model = m;
+        amountOfFuel = aOF;
+        printData();
+    }
+    public Car(){ // Parameterless
+    brand = "";
+    model = "";
+    amountOfFuel = 0;
+    }
+
+    private void printData(){
+        System.out.println("Brand: " + brand);
+        System.out.println("Model: " + model);
+        System.out.println("Fuel: " + amountOfFuel);
+    }
+}
+*/
+/* Harjoitus 4
+class Car{
+    private String brand;
+
+    private String model;
+    public void setModel(String model){
+        this.model = model;
+    }
+    public String getModel(){
+        return model;
+    }
+
+    private int amountOfFuel;
+
+    public Car(String b, String m, int aOF){ // Parameterized
+        brand = b;
+        model = m;
+        amountOfFuel = aOF;
+        printData();
+    }
+    public Car(){ // Parameterless
+    brand = "";
+    model = "";
+    amountOfFuel = 0;
+    }
+
+    private void printData(){
+        System.out.println("Brand: " + brand);
+        System.out.println("Model: " + model);
+        System.out.println("Fuel: " + amountOfFuel);
+    }
+
+    public void refuel(int refuel){
+        int afterrefuel = amountOfFuel + refuel;
+        System.out.println("Fuel in the tank: " + amountOfFuel);
+        System.out.println("Refuel: " + refuel);
+        System.out.println("Fuel in the tank after the refuel: " + afterrefuel);
+        
+    }
+}
+*/
