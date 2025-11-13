5f4f363f4f91b5112d31b80b814182782a2300fe
Jogis <ronipihajoki@proton.me>
2025-11-13 20:32:20 +0200

    Harj 1 ja Harj 2

--- CodeRunner1-1/src/App.java
+++ CodeRunner1-1/src/App.java
@@ -1,21 +1,51 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        car olio1 = new car("Kia", "Rio", 2);
+        Car olio1 = new Car("Kia", "Rio", 2);
 
         olio1.accelerate();
         olio1.brake();
         olio1.printData();
     }
 } // Main loppu
-class car{
+/* Harjoitus 1
+class Car{
     public String brand;
     public String model;
     public int amountOfFuel;
     
-    public car(String b, String m, int aOF){
+    public void brake(){
+        System.out.println("Car is breaking");
+    }
+
+    public void accelerate(){
+        amountOfFuel --;
+        if(amountOfFuel > 0){
+            System.out.println("Car is accelerating");
+        }
+    }
+
+    public void printData(){
+        System.out.println("Brand: " + brand);
+        System.out.println("Model: " + model);
+        System.out.println("Fuel: " + amountOfFuel);
+    }
+}
+*/
+class Car{
+    public String brand;
+    public String model;
+    public int amountOfFuel;
+    
+    public Car(){ // Parameterless
+    brand = "";
+    model = "";
+    amountOfFuel = 0;
+    }
+
+    public Car(String b, String m, int aOF){ // Parameterized
         brand = b;
         model = m;
-        amountOfFuel=aOF;
+        amountOfFuel = aOF;
     }
     public void brake(){
         System.out.println("Car is breaking");
